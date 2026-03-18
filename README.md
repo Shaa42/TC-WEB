6dc14997ae52e96bc26e0c1dda1d110ef360a3be
Shaa42 <ryanvigoureux@hotmail.com>
2026-03-18 11:00:00 +0100

    FEAT: Added basic GET endpoint

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # TC-WEB
+
+## Frontend
+install packages
+```sh
+npm install
+```
+
+run in debug mode
+```sh
+npm run dev
+```
+
+run in build mode
+```sh
+npm run build
+```
+
+
+## Backend
+
+run backend server
+```sh
+cd backend
+go run cmd/main.go
+```
--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello")
+	// Create a Gin router
+	r := gin.Default()
+
+	// Trust only local
+	if err := r.SetTrustedProxies([]string{"127.0.0.1", "::1"}); err != nil {
+		panic(err)
+	}
+
+	r.GET("/", handleInit)
+	r.Run()
+}
+
+func handleInit(c *gin.Context) {
+	c.String(http.StatusOK, "Hello World\n")
 }
